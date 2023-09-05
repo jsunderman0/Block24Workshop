2458275f28eac934608b52728799a74778de60e8
Jack Sunderman <jacksunderman@Jacks-MacBook-Air.local>
2023-09-05 19:02:20 -0400

    finished (did not do css)

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,33 @@ import {puppyList} from './data.js'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
-  console.log(puppies)
+  
+
+  const [featPupId, setFeatPupId] = useState(null)
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
+
+  
 
   return (
     <>
-      <div>
+      <div className="App">
         {
           puppies.map((puppy) => {
-            return <p> {puppy.name}</p>
+            return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
           })
         }
+        { featPupId }
       </div>
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li> Age: {featuredPup.age}</li>
+            <li> Email: {featuredPup.emaol}</li>
+          </ul>
+        </div>
+      )}
       
     </>
   )
